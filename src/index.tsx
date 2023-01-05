3b45c4b452b74c220672890f474567440d9c87c0
ksib7 <sibirkin70@gmail.com>
2023-01-06 00:59:16 +0300

    removed react context

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { GlobalProvider } from "./components/context/GlobalState";
 import { App } from "./App";
 
 const root = ReactDOM.createRoot(
@@ -9,8 +8,6 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <GlobalProvider>
-      <App />
-    </GlobalProvider>
+    <App />
   </React.StrictMode>
 );
